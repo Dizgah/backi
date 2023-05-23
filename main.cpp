41136e8ba8640c308e4f4d20b6fce64bdd2e7f67
Dizgah <mhdizgah@gmail.com>
2023-05-23 11:32:46 +0430

    adding directory validation

--- main.cpp
+++ main.cpp
@@ -3,25 +3,33 @@
 
 int main(int argc, char* argv[])
 {
-    if( argc < 2)
+    if( argc < 3)
     {
-        std::cerr << "Error: insufficient argument has been provided!";
-//        return -1;
+        std::cerr << "Error: insufficient argument has been provided !" << std::endl;
+        return -1;
     }
 
     std::vector< std::string_view> arguments;
 
-    for( size_t cntr = 0; cntr < argc; cntr++)
+    for( size_t cntr = 1; cntr < argc; cntr++)
     {
-         arguments.push_back(argv[cntr]);
+        arguments.push_back(argv[cntr]);
     }
 
-    for (size_t cntr = 0; cntr < arguments.size(); cntr++)
+    std::filesystem::directory_entry dir;
+    for( size_t cntr = 0; cntr < arguments.size(); cntr++)
     {
-        std::cout << arguments.at(cntr) << std::endl;
+        dir.assign( arguments.at( cntr));
+        //std::cout << arguments.at( cntr) << std::endl;
+
+        if( !dir.is_directory())
+        {
+            std::cerr << "Error: " << arguments.at(cntr) << " directory is Invalid !" << std::endl;
+
+            return -1;
+        }
     }
 
-//    BackiCore bk("./dir2", "./dir1");
     BackiCore bk( arguments.at( 0), arguments.at( 1));
 
     bk.makeCpyList();
